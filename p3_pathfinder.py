d0bbfe27b0a0a03182a3a4b0e45dfa0d38a7c573
mgurung <mgurung@ucsc.edu>
2015-04-20 20:53:08 -0700

    bfs works (it's pretty slow)

--- p3_pathfinder.py
+++ p3_pathfinder.py
@@ -1,8 +1,9 @@
-
+import Queue
 
 def find_path(src, dst, mesh):
 	
 	path = []
+	srcdstPath = []
 	visited = []
 	adj = mesh['adj']
 	boxes = mesh['boxes']
@@ -10,26 +11,56 @@ def find_path(src, dst, mesh):
 	dstFound = 0
 	sy, sx = src
 	dy, dx = dst
+	srcBox = None
+	dstBox = None
+	hasPath = 0
 	
+	#Identify the source and destination boxes
 	for box in boxes:
 		x1, x2, y1, y2 = box
 		
 		if sx < x2 and sy < y2 and sx > x1 and sy > y1:
-			#print src
-			path.append((src,dst))
+			#print adj[box]
+			path.append((src, dst))
+			srcBox = box
 			srcFound = 1
 		
 		if dx > x1 and dy > y1 and dx < x2 and dy < y2:
 			#print dst
-			path.append((src,dst))
+			path.append((src, dst))
+			dstBox = box
 			dstFound = 1
 			
 		if srcFound == 1 and dstFound == 1:
 			break
-		
-		
-		
 			
-	print path
+	
+	#Implement the simplest complete search algorithm you can
+	
+	q = Queue.Queue()
+	discovered = []
+	q.put(srcBox)
+	discovered.append(srcBox)
+	while not q.empty():
+		node = q.get()
+		if node == dstBox:
+			hasPath = 1
+			break
+		for edge in adj[node]:
+			if edge not in discovered:
+				q.put(edge)
+				discovered.append(edge)
+		
+	if hasPath < 1:
+		print "No Path Found"
+		
+	
+	#Modify your simple search to compute a legal list of line segments demonstrating the path
+	
+	#Modify your simple search to implement Dijkstra's algorithm
+	
+	#Modify your Dijkstra's implementation into an A* implementation
+	
+	#Modify your Dijkstra's (or A*) into a bidirectional Dijkstra's (or bidirectional A*)
 	
 	return path, visited
