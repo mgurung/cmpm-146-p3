30a0cf6be2ac474553cc6138ed6d21806b0bd699
mgurung <mgurung@ucsc.edu>
2015-04-21 15:25:53 -0700

    Fixed stuff

--- p3_pathfinder.py
+++ p3_pathfinder.py
@@ -3,7 +3,6 @@ import Queue
 def find_path(src, dst, mesh):
 	
 	path = []
-	srcdstPath = []
 	visited = []
 	adj = mesh['adj']
 	boxes = mesh['boxes']
@@ -11,69 +10,83 @@ def find_path(src, dst, mesh):
 	srcFound = 0
 	dstFound = 0
 	sy, sx = src
-	SRC = sx, sy
 	dy, dx = dst
 	srcBox = None
 	dstBox = None
 	hasPath = 0
 	diffX = 0
 	diffY = 0
-	
+	#print 'working'
 	#Identify the source and destination boxes
 	for box in boxes:
 		x1, x2, y1, y2 = box
-		
-		if sx < x2 and sy < y2 and sx > x1 and sy > y1:
-			#print adj[box]
+		#print 'working'
+		if sx < y2 and sy < x2 and sx > y1 and sy > x1:
+			print 'found src'
 			#path.append((src, dst))
 			diffX = x2 - sx
 			diffY = y2 - sy
-			detail_points[box] = sx, sy
 			srcBox = box
+			detail_points[srcBox] = sy, sx
 			srcFound = 1
-		
-		if dx > x1 and dy > y1 and dx < x2 and dy < y2:
-			#print dst
-			#path.append((src, dst))
-			detail_points[box] = dx, dy
-			dstBox = box
-			dstFound = 1
-			
-		if srcFound == 1 and dstFound == 1:
-			break
 			
 	
+	for box in boxes:
+		x1, x2, y1, y2 = box
+		#print 'working'
+		if dx > y1 and dy > x1 and dx < y2 and dy < x2:
+			print 'found dst'
+			#path.append((src, dst))
+			dstBox = box
+			detail_points[dstBox] = (dy, dx)
+			dstFound = 1
+	
+	#print src
+	#print detail_points[srcBox]
 	#Implement the simplest complete search algorithm you can
 	
 	q = Queue.Queue()
-	discovered = []
+	#discovered = []
 	q.put(srcBox)
-	discovered.append(srcBox)
-	prev = None
+	visited.append(srcBox)
+	prev = detail_points[srcBox]
 	while not q.empty():
 		node = q.get()
 		
 		if node == dstBox:
 			hasPath = 1
+			path.append((prev, detail_points[dstBox]))
 			break
+			
+		y, x = detail_points[node]
+		
 		edges = adj[node]
 		for edge in edges:
-			if edge not in discovered:
+			if edge not in visited:
 				q.put(edge)
-				discovered.append(edge)
+				visited.append(edge)
 				nx1, nx2, ny1, ny2 = edge
-				detail_points[edge] = nx2 - diffX, ny2 - diffY
+				detail_points[edge] = (min(ny2-1,max(ny1,y)), min(nx2-1,max(nx1,x)))
 		
-		if node == srcBox:
-			prev = detail_points[node]
-		elif prev != detail_points[node]:
+		
+		if prev != detail_points[srcBox]:
 			path.append((prev, detail_points[node]))
-		
+			
 		prev = detail_points[node]
+	'''	
+	prev = None
+	curr = None
+	for v in visited:
+		if !prev:
+			prev = detail_points[visited.pop()]
+			curr = detail_points[visited.pop()]
+		else:
+			prev = curr
+			curr = detail_points[visited.pop()]
 		
 	if hasPath < 1:
 		print "No Path Found"
-		
+	'''	
 	
 	#Modify your simple search to compute a legal list of line segments demonstrating the path
 	
